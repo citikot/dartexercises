c294dcbe5f93112a6baffaeb4685ce914051a572
Alex Pishvanov <citikot@dr.com>
2021-02-24 21:47:35 +0200

    sb block 2 task 2

--- sb_block2_t2.dart
+++ sb_block2_t2.dart
@@ -17,18 +17,28 @@ var hero = findUser(data, 'Iron man');
 print(hero); 
 */
 
-List<Map<String, dynamic>> data = [
+Map findUser(List<Map<String, dynamic>> data, String name) {
+  Map<String, dynamic> output = {'name': 'Anonimus', 'age': 0};
 
+  for (Map item in data) {
+    if (item.containsValue(name)) {
+      output = item;
+      break;
+    }
+  }
+  return output;
+}
+
+void main() {
+  List<Map<String, dynamic>> data = [
     {"name": "Batman", "age": 30},
-
     {"name": "Spiderman", "age": 18},
-
     {"name": "Kung fury", "age": 23},
-
     {"name": "Tor", "age": 550},
-
     {"name": "Halk", "age": 44},
-
     {"name": "Iron man", "age": 35},
+  ];
 
-];
+  var hero = findUser(data, "Iron Man");
+  print(hero);
+}
